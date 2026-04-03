4d755a5d26dbcb88fd9ce632dbe63b2f3f9b3f22
yosa12978 <yosa12978@gmail.com>
2026-04-04 00:09:08 +0300

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"errors"
+	"log"
+
+	"github.com/okunix/stash-sdk/stash/v1"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "stashctl",
+	Short: "stash secrets manager cli tool",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		serverAddr := viper.GetString("server")
+		username := viper.GetString("username")
+		password := viper.GetString("password")
+		var client *stash.Client
+		var err error
+		if username != "" || password != "" {
+			client, err = stash.NewClient(
+				stash.WithAddr(serverAddr),
+				stash.WithUser(username, password),
+			)
+		} else {
+			client, err = stash.NewClient(stash.WithAddr(serverAddr))
+		}
+		if err != nil {
+			return err
+		}
+		if err := client.Ping(ctx); err != nil {
+			return errors.New("failed to reach server")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+var cfgFile string
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "set different config file")
+}
+
+func Execute() {
+	cobra.OnInitialize(func() {
+		viper.SetEnvPrefix("STASH")
+		viper.AutomaticEnv()
+		viper.SetDefault("server", "http://localhost:7878")
+		viper.SetDefault("version", "v1")
+
+		if cfgFile != "" {
+			viper.AddConfigPath(cfgFile)
+		} else {
+			viper.SetConfigName("stashctl")
+			viper.SetConfigType("yaml")
+			viper.AddConfigPath(".")
+			viper.AddConfigPath("$XDG_CONFIG_HOME/stash/")
+			viper.AddConfigPath("$HOME/.config/stash/")
+			viper.AddConfigPath("$HOME/.stash/")
+			viper.AddConfigPath("/etc/stash/")
+		}
+		if err := viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+				log.Fatal(err.Error())
+			}
+		}
+	})
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/okunix/stashctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
